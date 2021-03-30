4f36ea37594e5a135a8cedabfe0dde5b34e2a77a
NooryA <apsarnoor@gmail.com>
2021-03-30 19:04:46 -0400

    added small comment

--- project/Ball.v
+++ project/Ball.v
@@ -20,35 +20,36 @@ begin
 		counter <= 25'd0;
 		x <= x + speedX;
 		y <= y + speedY;
-		
+
 		if(y>= 480)
 		begin
 		speedY <= -1;
 		end
-		
+
 		if(y<=10)
 		begin
 		speedY <=  10'd1;
 		end
-		
+
 		if(x>= 580)
 		begin
 		speedX <= -1;
 		end
-		
+
 		if(x<=70)
 		begin
 		speedX <= 10'd1;
 		end
-		
-		
-		
 
-	
+		//This is the Noor Branch
+
+
+
+
 	end
-	
-	
-	
+
+
+
 	intersect <= (hsp >= x - 10'd5 && hsp < x + 10'd5) && (vsp >= y - 10'd5 && vsp < y + 10'd5);
 
 end
