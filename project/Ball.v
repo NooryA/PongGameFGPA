1c9c5ff916b8d12a983a5f52d4901367e9d9ff09
NooryA <apsarnoor@gmail.com>
2021-03-30 20:57:31 -0400

    made ball reset when game over and bounce off paddle

--- project/Ball.v
+++ project/Ball.v
@@ -5,12 +5,12 @@ input [9:0] hsp, vsp;
 
 reg [9:0] x = 10'd320;
 reg [9:0] y = 10'd240;
-reg [9:0] speedX = 10'd1;
+reg [9:0] speedX = 10'd5;
 reg [9:0] speedY = 10'd1;
 output reg intersect = 1'b0;
 
 reg [24:0] counter = 25'd0;
-parameter MAX = 25'd100000;
+parameter MAX = 25'd500000;
 
 always @(posedge clk)
 begin
@@ -21,27 +21,50 @@ begin
 		x <= x + speedX;
 		y <= y + speedY;
 
-		if(y>= 480)
+		if (x >= 580 && y >= 180 && y <= 280) // bounces off the paddle
+               begin 
+            speedX <= -5;
+            
+                end
+	else if (x<= 70 && y >= 180 && y <= 280) // bounces off the paddle
 		begin
+				speedX <= 5;
+				
+		end
+		
+		else if(y>= 480)                    // bounces off screen
+		begin
+		speedY <= -5;
+	
+		end
+
+		else if(y<=10)                     // bounces off screen
+		begin
+	
+	speedY <=  10'd1;
+		end
+
+		else if(x>= 620)                   // resets ball in the middle
+		begin
+		x<= 320;
+		y <= 240;
+		speedX <= -5;
 		speedY <= -1;
+	
 		end
 
-		if(y<=10)
+		else if(x<=30)                      // resets ball in the middle
 		begin
-		speedY <=  10'd1;
-		end
-
-		if(x>= 580)
-		begin
-		speedX <= -1;
-		end
-
-		if(x<=70)
-		begin
-		speedX <= 10'd1;
-		end
+	   x<= 320;
+		y <= 240;
+	
+		speedX <= 10'd5;
+		speedY <= 1;
+		end 
 
 		//This is the Noor Branch
+
+
 
 
 
--- project/output_files/Pong.cdf
+++ project/output_files/Pong.cdf
@@ -4,7 +4,7 @@ JedecChain;
 	DefaultMfr(6E);
 
 	P ActionCode(Cfg)
-		Device PartName(10M50DAF484ES) Path("C:/Users/adam/Desktop/3216/Master Branch Final project/Pong-main/project/output_files/") File("Pong.sof") MfrSpec(OpMask(1));
+		Device PartName(10M50DAF484ES) Path("C:/Users/adam/Desktop/3216/Gittest#2/Pong-main/project/output_files/") File("Pong.sof") MfrSpec(OpMask(1));
 
 ChainEnd;
 
